580f57326addbc1ecffde6005eac250a66976d5f
viniduminsara <viniduminsara@gmail.com>
2023-08-16 16:18:54 +0530

    complete login page

--- src/main/java/com/d24/Appinitializer.java
+++ src/main/java/com/d24/Appinitializer.java
@@ -1,7 +1,11 @@
 package com.d24;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Appinitializer extends Application {
 
@@ -10,7 +14,10 @@ public class Appinitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/loginform.fxml"))));
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("Login");
+        primaryStage.show();
     }
 }
