225a7126558a47bb1f0ce61dd019fe6df8b3d8f2
viniduminsara <viniduminsara@gmail.com>
2023-08-18 21:45:37 +0530

    add Student page

--- src/main/java/com/d24/controller/DashboardFormController.java
+++ src/main/java/com/d24/controller/DashboardFormController.java
@@ -1,9 +1,12 @@
 package com.d24.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,5 +24,37 @@ public class DashboardFormController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    void btnAccountOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        contentPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/homeform.fxml")));
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) contentPane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/loginform.fxml"))));
+        stage.show();
+    }
+
+    @FXML
+    void btnReservationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRoomsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnStudentOnAction(ActionEvent event) throws IOException {
+        contentPane.getChildren().add(FXMLLoader.load(getClass().getResource("/view/studentForm.fxml")));
     }
 }
