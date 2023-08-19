440446a197ae7201134c83152ecb74de9a61f808
viniduminsara <viniduminsara@gmail.com>
2023-08-19 06:30:33 +0530

    change Student page

--- src/main/java/com/d24/controller/StudentFormController.java
+++ src/main/java/com/d24/controller/StudentFormController.java
@@ -1,4 +1,21 @@
 package com.d24.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StudentFormController {
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/popup/addStudentForm.fxml"))));
+        stage.setTitle("Add new student");
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
